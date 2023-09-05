f7a8e862aaf9d5339a6786f7f17ddabe0a106c6c
abda-torey <abdatorey@gmail.com>
2023-09-05 19:33:52 +0100

    changes img to Image

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -1,31 +1,35 @@
-'use client';
-import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-
+"use client";
+import { useState } from "react";
+import { Dialog } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
 const navigation = [
-  { name: 'Home', href: '#' },
-  { name: 'Services', href: '#' },
-  { name: 'About', href: '#' },
-  { name: 'Contact', href: '#' },
- 
-]
+  { name: "Home", href: "#" },
+  { name: "Services", href: "#" },
+  { name: "About", href: "#" },
+  { name: "Contact", href: "#" },
+];
 
 export default function HeroSection() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <div className="bg-white">
       <header className="absolute inset-x-0 top-0 z-50">
         <div className="mx-auto max-w-7xl">
           <div className="px-6 pt-6 lg:max-w-2xl lg:pl-4 lg:pr-0">
-            <nav className="flex items-center justify-between lg:justify-start" aria-label="Global">
+            <nav
+              className="flex items-center justify-between lg:justify-start"
+              aria-label="Global"
+            >
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
-                <img
-                  alt="Your Company"
-                  className="h-8 w-auto"
-                  src="logo.svg"
+                <Image
+                  src="/logo.svg"
+                  alt="logo"
+                  width={100}
+                  height={100}
+                  className="h-9 w-auto"
                 />
               </a>
               <button
@@ -38,25 +42,37 @@ export default function HeroSection() {
               </button>
               <div className="hidden lg:ml-12 lg:flex lg:gap-x-14">
                 {navigation.map((item) => (
-                  <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="text-sm font-semibold leading-6 text-gray-900"
+                  >
                     {item.name}
                   </a>
                 ))}
-                
               </div>
             </nav>
           </div>
         </div>
-        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <Dialog
+          as="div"
+          className="lg:hidden"
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+        >
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
-                <img
-                  className="h-8 w-auto"
-                  src="logo.svg"
-                  alt=""
+
+                
+                <Image
+                  src="/logo.svg"
+                  alt="logo"
+                  width={100}
+                  height={100}
+                  className="h-9 w-auto"
                 />
               </a>
               <button
@@ -109,14 +125,13 @@ export default function HeroSection() {
 
             <div className="relative px-6 py-32 sm:py-40 lg:px-8 lg:py-56 lg:pr-0">
               <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
-                <div className="hidden sm:mb-10 sm:flex">
-                 
-                </div>
+                <div className="hidden sm:mb-10 sm:flex"></div>
                 <h1 className="text-4xl text- font-bold tracking-tight text-gray-900 sm:tex-6xl">
-                    Turning Beginners Into Confident Drivers
+                  Turning Beginners Into Confident Drivers
                 </h1>
                 <p className="mt-6 text-lg leading-8 text-gray-600">
-                Whether you are a first-time learner or need to brush up your driving skills, you can rely on us.
+                  Whether you are a first-time learner or need to brush up your
+                  driving skills, you can rely on us.
                 </p>
                 <div className="mt-10 flex items-center gap-x-6">
                   <a
@@ -125,7 +140,10 @@ export default function HeroSection() {
                   >
                     Get started
                   </a>
-                  <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                  <a
+                    href="#"
+                    className="text-sm font-semibold leading-6 text-gray-900"
+                  >
                     Learn more <span aria-hidden="true">→</span>
                   </a>
                 </div>
@@ -134,13 +152,15 @@ export default function HeroSection() {
           </div>
         </div>
         <div className="bg-gray-50 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-          <img
-            className="aspect-[3/2] object-cover lg:aspect-auto lg:h-full lg:w-full"
-            src="/hero.png"
-            alt=""
+          <Image
+            width={500}
+            height={500}
+            alt="heroImage"
+            src="/driverHero.png"
+            className="aspect-[3/2] object-cover w-full h-auto lg:aspect-auto lg:h-full lg:w-full"
           />
         </div>
       </div>
     </div>
-  )
+  );
 }
